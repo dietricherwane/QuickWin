c2013d7704b83106ac46d30f202935f353b4e200
Jean TOURE <touremjean@hotmail.com>
2019-04-12 00:20:17 +0000

    changes

--- app/controllers/customer_transactions_controller.rb
+++ app/controllers/customer_transactions_controller.rb
@@ -3,7 +3,7 @@ class CustomerTransactionsController < ApplicationController
 
   def list
     @transactions_active = "active"
-    @transactions = session[:customer].sms_transactions.order("created_at DESC")
+    @transactions = Customer.find_by_id(session[:customer]).sms_transactions.order("created_at DESC")
     @transactions_count = @transactions.count
     @transactions = @transactions.page(params[:page])
   end
